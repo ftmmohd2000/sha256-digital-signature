4bc3ac196cda8b2ee6814bcf8341ddbe45eb3038
Mohammed M <ftmmohd2000@gmail.com>
2020-11-27 03:48:09 +0530

    added public key display to final output

--- main.cpp
+++ main.cpp
@@ -9,14 +9,16 @@ int main()
      string message = "Hello World";
 
      cout << '\n'
-          << "The message is:" << '\n' << '\n'
+          << "The message is:" << '\n'
+          << '\n'
           << message << '\n';
 
      cout << "----------------------------------------------------------------" << '\n';
 
      // here the message is hashed according to the sha256 specification
      string hashedMessage = sha256(message);
-     cout << "The hash of this message is:" << '\n' << '\n'
+     cout << "The hash of this message is:" << '\n'
+          << '\n'
           << hashedMessage << '\n';
 
      cout << "----------------------------------------------------------------" << '\n';
@@ -33,17 +35,24 @@ int main()
      ZZ signature;
      sign(signature, pt, priv, N);
 
-     cout << "This hash is signed with the private key to obtain:" << '\n' << '\n'
+     cout << "This hash is signed with the private key to obtain:" << '\n'
+          << '\n'
           << toHex(signature) << '\n';
+
+     cout << "----------------------------------------------------------------" << '\n';
+
+     cout << "The public key that can be used for verification is:" << '\n'
+          << pub << '\n';
 
      cout << "----------------------------------------------------------------" << '\n';
 
      // Verification process
      cout << "This signature can be 'decrypted' using the public key to obtain" << '\n'
-      
+
           << "the hash of the message. The original message can now be hashed" << '\n'
           << "and this can be compared with the decrypted value from the sign" << '\n'
-          << "to verify message authenticity:" << '\n' << '\n';
+          << "to verify message authenticity:" << '\n'
+          << '\n';
      cout << (verify(message, signature, pub, N) ? "verified!" : "not verified!") << '\n';
 
      cout << endl;
